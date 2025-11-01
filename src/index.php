66fd7bafd4cd68807dc62e630e3bee4ddfe6e12a
maritha <muhorakeyemartha5@gmail.com>
2025-11-01 22:32:56 +0200

    index page is created

---
+++ src/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Share Ride App</title>
+</head>
+<body>
+    <h1>Welcome to ShareRide</h1>
+    <a href="registration.php">Register</a> |
+    <a href="login.php">Login</a>
+</body>
+</html>
